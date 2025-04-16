5b1a052173c7bd986149fec715daf9bee23b88ca
Jaya priya <shahisaumya330@gmail.com>
2025-04-16 08:07:33 +0530

    Fix setup.sh and Procfile for Render

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
-mkdir -p ~/.streamlit/
+#!/bin/bash
+
+mkdir -p $HOME/.streamlit/
 
 echo "\
 [server]\n\
-port = $port\n\
+port = $PORT\n\
 enableCORS = false\n\
 headless = true\n\
-\n\
-" > ~/ .streamlit/config.toml
+" > $HOME/.streamlit/config.toml
